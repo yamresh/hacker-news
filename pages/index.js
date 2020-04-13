3e34b12a9a59e460d6000f613db2c6fcad6cc105
Amresh Yadav <amresh.yadav@publicissapient.com>
2020-04-13 09:35:35 +0530

    fix : change

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import fetch from 'isomorphic-unfetch';
-import App from '../components/app';
-import NewsList from '../components/NewsList/NewsList';
+import fetch from "isomorphic-unfetch";
+import App from "../components/app";
+import NewsList from "../components/NewsList/NewsList";
 
-const Index = props => (
-    <App>
-        <NewsList {...props} />
-    </App>
+const Index = (props) => (
+  <App>
+    <NewsList {...props} />
+  </App>
 );
 
 Index.getInitialProps = async function (context) {
-    const { page } = context.query || {};
-    let url = 'https://hn.algolia.com/api/v1/search?tags=front_page';
-    if (page) {
-        url = `${url}&page=${page}`;
-    }
-    const res = await fetch(url);
-    const data = await res.json();
+  const { page } = context.query || {};
+  let url = "https://hn.algolia.com/api/v1/search?tags=front_page";
+  if (page) {
+    url = `${url}&page=${page}`;
+  }
+  const res = await fetch(url);
+  const data = await res.json();
 
-    return {
-        newsData: data || {},
-    };
+  return {
+    newsData: data || {},
+  };
 };
 
 export default Index;
